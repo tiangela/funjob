f691e60917f42a8fa68d2c17283b6e5519faa50f
tiangela <tas14@yandex.ru>
2018-04-09 19:44:09 +0300

    js с одной карточкой

--- js/script.js
+++ js/script.js
@@ -1,94 +1,65 @@
-'use strict'
+var catItem = document.querySelector('.catalog__item');
+var catBuyItems = document.querySelectorAll('.catalog__order-text');
+var catApproveItems = document.querySelectorAll('.catalog__motto');
+var catItemSelected = document.querySelector('.catalog__item--selected');
+var catTextDef = document.querySelector('.catalog__order-text--default');
+var catTextSel = document.querySelector('.catalog__order-text--selected');
+var catTextDis = document.querySelector('.catalog__order-text--disabled');
+var catChoice = document.querySelector('.catalog__order-link');
+var catTextNoHover = document.querySelector('.catalog__motto--active');
+var catTextHover = document.querySelector('.catalog__motto--hover');
 
-var catItems = document.querySelectorAll(".catalog__item");
-var catChoices = document.querySelectorAll(".catalog__order-link");
-var catTextDef = document.querySelector(".catalog__order-text--default");
-var catTextSel = document.querySelector(".catalog__order-text--selected");
-var catTextDis = document.querySelector(".catalog__order-text--disabled");
-var catTextNoHover = document.querySelector(".catalog__motto--active");
-var catTextHover = document.querySelector(".catalog__motto--hover");
-
-/*catItems.forEach(function(item) {
-item.addEventListener("click", function() {
-  if (item.classList.contains("catalog__item--selected")) {
-    item.classList.remove("catalog__item--selected");
-    item.classList.add("catalog__item--default");
-    catTextSel.style.display = 'none';
-    catTextDef.style.display = 'block';
-
-} else {
-    item.classList.add("catalog__item--selected");
-    item.classList.remove("catalog__item--default");
+catItem.addEventListener('click', function() {
+  if (catItem.classList.contains('catalog__item--hover')) {
+    catItem.classList.remove('catalog__item--hover');
+    catTextHover.style.display = 'none';
+    catTextNoHover.style.display = 'block';
+  }
+  if (catItem.classList.contains('catalog__item--default')) {
+    catItem.classList.toggle('catalog__item--selected');
+    catItem.classList.remove('catalog__item--default');
     catTextDef.style.display = 'none';
     catTextSel.style.display = 'block';
-  }
-});
-});
-
-catChoices.forEach(function(choice) {
-choice.addEventListener("click", function() {
-  catItems.forEach(function(item) {
-  if (item.classList.contains("catalog__item--selected")) {
-    item.classList.remove("catalog__item--selected");
-    item.classList.add("catalog__item--default");
-    catTextSel.style.display = 'none';
-    catTextDef.style.display = 'block';
   } else {
-    item.classList.add("catalog__item--selected");
-    item.classList.remove("catalog__item--default");
-    catTextDef.style.display = 'none';
-    catTextSel.style.display = 'block';
-  }
-  });
-});
-}); */
-
-var onClickCard = function () {
-  catItems.forEach(function(item) {
-  if (item.classList.contains("catalog__item--selected")) {
-    item.classList.remove("catalog__item--selected");
-    item.classList.add("catalog__item--default");
+    catItem.classList.toggle('catalog__item--default');
+    catItem.classList.remove('catalog__item--selected');
     catTextSel.style.display = 'none';
     catTextDef.style.display = 'block';
+  }
+});
 
-} else {
-    item.classList.add("catalog__item--selected");
-    item.classList.remove("catalog__item--default");
+catChoice.addEventListener('click', function() {
+  if (catItem.classList.contains('catalog__item--hover')) {
+    catItem.classList.remove('catalog__item--hover');
+  }
+  if (catItem.classList.contains('catalog__item--default')) {
+    catItem.classList.toggle('catalog__item--selected');
+    catItem.classList.remove('catalog__item--default');
     catTextDef.style.display = 'none';
     catTextSel.style.display = 'block';
+  } else {
+    catItem.classList.toggle('catalog__item--default');
+    catItem.classList.remove('catalog__item--selected');
+    catTextSel.style.display = 'none';
+    catTextDef.style.display = 'block';
+    catTextNoHover.style.display = 'block';
   }
-  });
-};
+});
 
-
-for (var i = 0; i < catItems.length; i++) {
-  catItems[i].addEventListener('click', onClickCard);
-  catChoices[i].addEventListener('click', onClickCard);
-}
-
-catItems.forEach(function(card) {
-card.addEventListener("mouseenter", function() {
-  if (card.classList.contains("catalog__item--selected")) {
-    card.classList.remove("catalog__item--selected");
-    card.classList.add("catalog__item--hover");
+catItem.addEventListener('mouseenter', function() {
+  if (catItem.classList.contains('catalog__item--selected')) {
+    catItem.classList.remove('catalog__item--selected');
+    catItem.classList.add('catalog__item--hover');
     catTextNoHover.style.display = 'none';
     catTextHover.style.display = 'block';
-}
-});
-card.addEventListener("mouseleave", function() {
-  if (card.classList.contains("catalog__item--hover")) {
-    card.classList.remove("catalog__item--hover");
-    card.classList.add("catalog__item--selected");
-    catTextNoHover.style.display = 'block';
-    catTextHover.style.display = 'none';
-}
-});
+  }
 });
 
-for (var i = 0; i < catItems.length; i++) {
-  if (catItems[i].classList.contains("catalog__item--disabled")) {
-    catTextSel.style.display = 'none';
-    catTextDef.style.display = 'none';
-    catTextDis.style.display = 'block';
+catItem.addEventListener('mouseleave', function() {
+  if (catItem.classList.contains('catalog__item--hover')) {
+    catItem.classList.remove('catalog__item--hover');
+    catItem.classList.add('catalog__item--selected');
+   catTextHover.style.display = 'none';
+   catTextNoHover.style.display = 'block';
   }
-}
+});
